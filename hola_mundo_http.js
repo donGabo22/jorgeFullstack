80f596ec2dbd417efe16b225a5195a7d125a23b1
Gabriel Hernandez Diaz <gabrielhdz266@gmail.com>
2023-04-21 09:54:48 -0600

    Cambio de cabecera de http a html

--- hola_mundo_http.js
+++ hola_mundo_http.js
@@ -3,8 +3,8 @@ const hostname = '192.168.1.14';
 const port = 3000;
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hola Mundo\n');
+    res.setHeader('Content-Type','text/html');
+    res.end('<p>gabriel desde vc con git </p>');
 });
 
 server.listen(port, hostname, () => {
